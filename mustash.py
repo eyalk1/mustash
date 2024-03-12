b7e614b55c29f194eab79a6562a2728b4cca50e3
Eyal Kamitchi <eyalkamitchi1@gmail.com>
2024-03-12 23:40:19 +0200

    WIP naking used work - next is query
    Signed-off-by: Eyal Kamitchi <eyalkamitchi1@gmail.com>

--- mustash.py
+++ mustash.py
@@ -3,6 +3,10 @@ import json
 import os
 import pdb
 from playsound import playsound
+from audio_formats import AUDIO_FORMATS, SCORE_FORMATS
+from emotions import EMOTIONS
+from functools import partial
+from itertools import chain
 
 REC_FILENAME = "recs.json"
 PROMPT = ">>> "
@@ -11,14 +15,23 @@ TEMPI = ["fast", "mid", "slow"]
 """
     TODO:
 1. make input nicer
-2. catch exceptions
+    2. handle used
 3. add search and view
+5. split to files
 4. when lsing print everything that is relevant(i.e. view)
 """
 
 
+def try_until_good(func, *args, **kwargs):
+    while True:
+        try:
+            return func(*args, **kwargs)
+        except ValueError:
+            print("try again")
+
+
 def get_tempo():
-    return get_from_options("tempo", ["slow", "medium", "fast"])
+    try_until_good(get_from_options, "tempo", ["slow", "medium", "fast"])
 
 
 def get_files_from_curdir(filetype: str, pred):
@@ -31,7 +44,7 @@ def get_files_from_curdir(filetype: str, pred):
             os.listdir(),
         )
     )
-    return get_from_options(filetype, files,accept_empty=True)
+    return get_from_options(filetype, files, accept_empty=True)
 
 
 def get_chords():
@@ -39,37 +52,81 @@ def get_chords():
 
 
 def get_feels():
-    # TODO: change to get from options and add energy level
-    return get_until_empty("feels")
+    keys_list = list(EMOTIONS.keys())
+    choices = []
+    while True:
+        choice = get_from_options("feels", keys_list, accept_empty=True)
+        if choice == "":
+            break
+        pdb.set_trace()
+        choices.append(get_from_options(choice, EMOTIONS[choice], accept_empty=True))
+        if choices[-1] == "":
+            choices = choices[:-1]
+            break
+
+    return choices
 
 
 def get_time_signature():
-    return get_until_empty("time signature")
+    def check_signature(sig):
+        if sig.count("/") != 1:
+            print(f"sig - {sig} is badly formed - use a single /")
+            return False
+        up, down = sig.split("/")
+        possible_denominators = ["2", "4", "8", "16", "32"]
+        if down not in possible_denominators:
+            print(
+                f"sig - {sig} is badly formed - the denominator should be one of {possible_denominators}"
+            )
+            return False
+        return True
+
+    while True:
+        sigs: list[str] = get_until_empty("time signature")
+        if all(map(check_signature, sigs)):
+            break
+
+    return sigs
+
+
+def get_length():
+    return float(input(f"{PROMPT}length - "))
+
+
+def check_audio_file(rec_file, extension_list, filename):
+    return (
+        filename not in [s["audio_file"] for s in rec_file]
+        and os.path.splitext(filename)[1] in extension_list
+    )
+
 
 def add_new_composition(rec_file):
     tempo = get_tempo()
     audio_file = get_files_from_curdir(
-        "audio_file",
-        lambda filename: filename
-        not in [s["audio_file"] for s in rec_file],
+        "audio_file", partial(check_audio_file, rec_file, AUDIO_FORMATS)
     )
     score_file = get_files_from_curdir(
-        "score_file",
-        lambda filename: filename
-        not in [s["score_file"] for s in rec_file],
+        "score_file", partial(check_audio_file, rec_file, SCORE_FORMATS)
     )
     chords = get_chords()
     has_line = (
         True
-        if get_from_options("has line", ["yes", "no"], default='no') == "yes"
+        if get_from_options("has line", ["no", "yes"], default="no") == "yes"
         else False
     )
     feel = get_feels()
     composer = get_from_options(
-        "composer", [rec["composer"] for rec in rec_file], add_new=True
+        "composer", list(set(rec["composer"] for rec in rec_file)), add_new=True
     )
-    used = input(">>> used - ")
-    length = int(input(">>> length - "))
+    used = get_from_options(
+        "compsitions using this progression",
+        chain(*map(lambda rec: rec["used"], rec_file)),
+        add_new=True,
+        accept_empty=True,
+        allow_multiple=True,
+    )
+    # used = set(get_until_empty("compsitions using this progression", chain(*map(lambda rec: rec["used"], rec_file))))
+    length = try_until_good(get_length)
     time_signature = get_time_signature()
     return {
         "tempo": tempo,
@@ -84,17 +141,25 @@ def add_new_composition(rec_file):
         "time_signature": time_signature,
     }
 
+
 def get_recs_chords(recs):
     return [r["chords"] for r in recs]
-    
+
+
 def save_recs(recs):
     json.dump(recs, open(REC_FILENAME, "w"))
-    
+
+
+def print_help():
+    print(
+        """\tls - see all recordings\n\tsearch - query for recordings\n\tadd - add new recording\n\tsave - save to db\n\trm - delete recording(only from db)\n\tplay - play recording\n\tedit - edit recrding's metadata\n\tq - quit\n\thelp - this help"""
+    )
+
 
 def main():
     rec_file = json.load(open(REC_FILENAME)) if os.path.isfile(REC_FILENAME) else []
 
-    cmd = ''
+    cmd = ""
     while cmd != "q":
         cmd = input(PROMPT).strip()
         match cmd:
@@ -107,7 +172,7 @@ def main():
                 rec_file.append(add_new_composition(rec_file))
             case "rm":
                 delete = get_from_options("recs", get_recs_chords(rec_file))
-                rec_file = list(filter(lambda rec:rec["name"] != delete, rec_file))
+                rec_file = list(filter(lambda rec: rec["name"] != delete, rec_file))
             case "play":
                 play = get_from_options("recs", get_recs_chords(rec_file))
                 for rec in rec_file:
@@ -118,9 +183,11 @@ def main():
             case "save":
                 save_recs(rec_file)
             case "help":
-                print("""\tls - see all recordings\n\tsearch - query for recordings\n\tadd - add new recording\n\tsave - save to db\n\trm - delete recording(only from db)\n\tplay - play recording\n\tedit - edit recrding's metadata\n\tq - quit\n\thelp - this help""")
+                print_help()
             case "q":
                 save_recs(rec_file)
+            case _:
+                print_help()
 
 
 def dowhile(action, check, accumulate=False):
@@ -135,38 +202,69 @@ def dowhile(action, check, accumulate=False):
     return thing
 
 
-def get_from_options(category_name, options, add_new=False, accept_empty=False,default=''):
-    if default not in options and default != '':
-        raise 5
+def get_from_options(
+    category_name,
+    options,
+    add_new=False,
+    accept_empty=False,
+    default="",
+    allow_multiple=False,
+):
+    if default not in options and default != "":
+        raise ValueError("default value has to be one of the options if it is given")
+    elif default != "" and default in options:
+        options[options.index(default)] = (
+            "[" + options[options.index(default)].upper() + "]"
+        )
     if add_new:
         options.append("new")
     prompt_prefix = f">>> {category_name} - "
-    optionlist = "\n".join([f"\t{n} - {'['if option == default else ''}{option}{']' if option == default else ''}" for n, option in enumerate(options)])
+    optionlist = "\n".join([f"\t{n} - {option}" for n, option in enumerate(options)])
     prompt = f"{prompt_prefix}\n{optionlist}\n{PROMPT}"
 
-    result = None
-    while True:
-        result = input(prompt)
-        if (default != '' or accept_empty) and result == '':
-                return default
-        result = int(result)
-        print(result)
-        if result < len(options):
-            if options[result] == "new":
-                return input(f"enter new {category_name} - ")
-            return options[int(result)]
-        print("try again retard")
+    def get_input(
+        category_name,
+        options,
+        add_new=False,
+        accept_empty=False,
+        default="",
+        allow_multiple=False,
+    ):
+        result = None
+        while True:
+            result = input(prompt)
+            if result == "":
+                if default != "" or accept_empty:
+                    return default
+                else:
+                    continue
+            if not result.isdigit():
+                continue
+            result = int(result)
+            if result < len(options):
+                if options[result] == "new":
+                    return input(f"enter new {category_name} - ")
+                return options[result]
+            print("try again retard")
+
+    if allow_multiple:
+        i = get_input()
+    else:
+        return get_input()
 
 
-def get_until_empty(category):
-    return list(filter(
-        lambda x: x != "",
-        dowhile(
-            lambda: input(f"{PROMPT}{category} - "),
-            lambda things: things[-1] == "",
-            True,
-        ),
-    ))
+def get_until_empty(category, options="", check=None):
+    options_list = "\n".join(options)
+    return list(
+        filter(
+            lambda x: x != "",
+            dowhile(
+                lambda: input(f"{PROMPT}{category} - \n\t{options_list}"),
+                lambda things: things[-1] == "",
+                True,
+            ),
+        )
+    )
 
 
 if __name__ == "__main__":
